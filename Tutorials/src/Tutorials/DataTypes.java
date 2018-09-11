20ec1bf9ecb5372a4820987fac5db2e96c4c4a70
Meshach ishaya <meshachm126@gmail.com>
2018-09-11 19:33:35 +0100

    Latest

---
+++ Tutorials/src/Tutorials/DataTypes.java
@@ -0,0 +1,48 @@
+package Tutorials;
+
+public class DataTypes {
+//	The Primitive Types
+//	Java defines eight primitive types of data: byte, short, int, long, char, float, double, and boolean. The primitive types are also commonly referred to as simple types.
+//
+//	These can be put in four groups:
+//
+//	Integers : This group includes byte, short, int, and long, which are for whole-valued signed numbers.
+//	Floating-point numbers : This group includes float and double, which represent numbers with fractional precision.
+//	Characters : This group includes char, which represents symbols in a character set, like letters and numbers.
+//	Boolean : This group includes boolean, which is a special type for representing true/false values.
+//	We will see the each types in details with example program in upcoming chapters.
+//
+//	Integers
+//	Java defines four integer types:
+//	byte, short, int, and long. All of these are signed, positive and negative values. Java does not support unsigned, positive-only integers. Many other computer languages support both signed and unsigned integers. However, Java’s designers felt that unsigned integers were unnecessary.
+//
+//	byte : It is 1 byte(8-bits) integer data type. Value range from -128 to 127. Default value zero. example: byte b=10;
+//
+//	short : It is 2 bytes(16-bits) integer data type. Value range from -32768 to 32767. Default value zero. example: short s=11;
+//
+//	int : It is 4 bytes(32-bits) integer data type. Value range from -2147483648 to 2147483647. Default value zero. example: int i=10;
+//
+//	long : It is 8 bytes(64-bits) integer data type. Value range from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807. Default value zero. example: long l=100012;
+//
+//	Floating-Point Types
+//	Floating-point numbers, also known as real numbers, are used when evaluating expressions that require fractional precision. For example, calculations such as square root, or transcendental
+//
+//	such as sine and cosine, result in a value whose precision requires a floating-point type.
+//
+//	float : It is 4 bytes(32-bits) float data type. Default value 0.0f. example: float ff=10.3f;
+//
+//	double : It is 8 bytes(64-bits) float data type. Default value 0.0d. example: double db=11.123;
+//
+//	Characters
+//	In Java, the data type used to store characters is char. However, C/C++ programmers beware: char in Java is not the same as char in C or C++. In C/C++, char is 8 bits wide.
+//
+//	char : It is 2 bytes(16-bits) unsigned unicode character. Range 0 to 65,535. example: char c=’a’;
+//
+//	Booleans
+//	Java has a primitive type, called boolean, for logical values. It can have only one of two possible values, true or false. This is the type returned by all relational operators, as in the case of a < b. boolean is also the type required by the conditional expressions that govern the control statements such as if and for.
+//
+//	Example: boolean b=true;
+
+
+
+}
